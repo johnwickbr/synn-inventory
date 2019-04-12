4456af2a34678e48b5cfbff95307d3eeda747a59
Syntasu <abouma18@hotmail.com>
2019-04-12 16:27:16 +0200

    database: add inventory_item join table definitions

--- sql/inventory.sql
+++ sql/inventory.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `inventory` (
-    `uiid` VARCHAR(255) NOT NULL PRIMARY KEY,
+    `uiid` VARCHAR(256) NOT NULL PRIMARY KEY,
     `owner` VARCHAR(64), 
     `theme` TINYINT(11) UNSIGNED NOT NULL DEFAULT 0,
     `style` TINYINT(11) UNSIGNED NOT NULL DEFAULT 0,
@@ -7,4 +7,13 @@ CREATE TABLE IF NOT EXISTS `inventory` (
     `height` TINYINT(11) UNSIGNED NOT NULL DEFAULT 8,
     `data` TEXT NOT NULL,
     INDEX idx_uiid_owner (uiid, owner)
+);
+
+CREATE TABLE IF NOT EXISTS `inventory_item` ( 
+    `id` INT NOT NULL AUTO_INCREMENT, 
+    `uiid` VARCHAR(256) NOT NULL, 
+    `x` TINYINT(11) UNSIGNED NOT NULL DEFAULT 0, 
+    `y` TINYINT(11) UNSIGNED NOT NULL DEFAULT 0, 
+    `itemid` INT(11) NOT NULL DEFAULT -1, 
+    PRIMARY KEY (`id`)
 );
